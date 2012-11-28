7a0f469f63be512707045c55ddf7621cb0c8e2d0
sebastian <sebastian@10.150.214.83>
2012-11-28 09:37:53 -0500

    first git commit

---
+++ SampleGitOne/src/slpz/sgitone/Maine.java
@@ -0,0 +1,21 @@
+package slpz.sgitone;
+
+public class Maine {
+
+	public static void main(String[] args) {
+
+		Twog two = new Twog();
+		
+		String name = "billy";
+		
+		//
+		
+		System.out.println("the git beginning");
+		
+		System.out.println(two.sayHello(name));
+		
+		System.out.println("the git end");
+		
+	}
+
+}
---
+++ SampleGitOne/src/slpz/sgitone/Twog.java
@@ -0,0 +1,9 @@
+package slpz.sgitone;
+
+public class Twog {
+	
+	public String sayHello(String name) {
+		return "Hello there "+ name +"!";
+	}
+	
+}
